6d229367e807ee2ed3332b4014f02e070d5e2ec6
Alyssa Hanson <alyghanson@gmail.com>
2025-04-22 09:45:37 -0700

    Finished toolbar (for now). Working links to 1. homepage 2. plant list 3. calender. logo and favicon updated. deleted react images.

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -5,8 +5,13 @@ import cat_photo from "./cat_logo.png"
 const Header = () => {
     return <div>
         <header className="header">
-            <img src={cat_photo} alt="cat photo" className="logo" />
+
+            <a href="/">
+                <img src={cat_photo} alt="cat photo" className="logo" />
+            </a>
+
             <p className="title">iNeedWater</p>
+            
             <nav>
                 <a href="/plantlist">Plant List</a>
                 <a href="/calendar">Calendar</a>
